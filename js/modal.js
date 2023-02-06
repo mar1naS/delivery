f9baa56cf247c6d5beae9c988fac637be63d58b7
Rinya7 <v.dmitriev0712@gmail.com>
2023-02-06 12:35:02 +0100

    pravki

--- js/modal.js
+++ js/modal.js
@@ -3,27 +3,20 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    openSecondModalBtn: document.querySelector("[data-second-modal-open]"), //селектори другого модального вікна
+    closeSecondModalBtn: document.querySelector("[data-second-modal-close]"), //селектори другого модального вікна
+    modalSecond: document.querySelector("[data-second-modal]"), //селектори другого модального вікна
+    form: document.querySelector("[form-modal]"), //це причепи на тег форм
   };
-
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
-
+  refs.openSecondModalBtn.addEventListener("click", toggleModal);
+  refs.closeSecondModalBtn.addEventListener("click", toggleModal);
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
-})();
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open-2]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  refs.form.addEventListener("submit", form);
+  function form(e) {
+    e.preventDefault();
   }
 })();
